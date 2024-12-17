ede64b9820a76ee08c7eb0a6fc8f580c486360b9
YeSuX <351490949@qq.com>
2024-12-17 22:01:36 +0800

    feat: add font family and heading level functionality to editor with toolbar support

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -12,6 +12,8 @@ import Image from '@tiptap/extension-image'
 import ImageResize from 'tiptap-extension-resize-image'
 import useEditorStore from '@/store/use-editor-store'
 import Underline from '@tiptap/extension-underline'
+import { FontFamily } from '@tiptap/extension-font-family'
+import TextStyle from '@tiptap/extension-text-style'
 
 const Editor = () => {
   const { setEditor } = useEditorStore()
@@ -62,7 +64,9 @@ const Editor = () => {
       TableRow,
       Image,
       ImageResize,
-      Underline
+      Underline,
+      FontFamily,
+      TextStyle
     ],
     content: `
     <table>
--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,9 +1,15 @@
 'use client'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu'
 import { Separator } from '@/components/ui/separator'
 import { cn } from '@/lib/utils'
 import useEditorStore from '@/store/use-editor-store'
 import {
   BoldIcon,
+  ChevronDownIcon,
   ItalicIcon,
   ListTodoIcon,
   LucideIcon,
@@ -15,6 +21,121 @@ import {
   UnderlineIcon,
   Undo2Icon
 } from 'lucide-react'
+
+import { type Level } from '@tiptap/extension-heading'
+
+const HeadingLevelButton = () => {
+  const { editor } = useEditorStore()
+
+  const headingLevels = [
+    { label: 'Normal', value: '0', fontSize: '16px' },
+    { label: 'Heading 1', value: '1', fontSize: '32px' },
+    { label: 'Heading 2', value: '2', fontSize: '24px' },
+    { label: 'Heading 3', value: '3', fontSize: '20px' },
+    { label: 'Heading 4', value: '4', fontSize: '18px' },
+    { label: 'Heading 5', value: '5', fontSize: '16px' }
+  ]
+
+  const getCurrentHeading = () => {
+    for (let level = 1; level < headingLevels.length; level++) {
+      if (editor?.isActive('heading', { level })) {
+        return `Heading ${level}`
+      }
+    }
+
+    return 'Normal'
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className='text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80'>
+          <span className='text-xs'>{getCurrentHeading()}</span>
+          <ChevronDownIcon className='w-3 h-3' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {headingLevels.map(({ label, value, fontSize }) => (
+          <button
+            key={value}
+            style={{ fontSize }}
+            className={cn(
+              'text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80',
+              (value === '0' && !editor?.isActive('heading')) ||
+                (editor?.isActive('heading', { level: Number(value) }) &&
+                  'bg-neutral-200/80')
+            )}
+            onClick={() => {
+              if (value === '0') {
+                editor?.chain().focus().setParagraph().run()
+              } else {
+                editor?.chain().focus().toggleHeading({ level: Number(value) as Level }).run()
+              }
+            }}
+          >
+            {label}
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
+
+const FontFamilyButton = () => {
+  const { editor } = useEditorStore()
+
+  const fonts = [
+    {
+      label: 'Arial',
+      value: 'Arial'
+    },
+    {
+      label: 'Times New Roman',
+      value: 'Times New Roman'
+    },
+    {
+      label: 'Courier New',
+      value: 'Courier New'
+    },
+    {
+      label: 'Georgia',
+      value: 'Georgia'
+    },
+    {
+      label: 'Impact',
+      value: 'Impact'
+    }
+  ]
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <button className='text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80'>
+          <span className='text-xs'>
+            {editor?.getAttributes('textStyle').fontFamily || 'Arial'}
+          </span>
+          <ChevronDownIcon className='w-3 h-3' />
+        </button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        {fonts.map(({ label, value }) => (
+          <button
+            key={value}
+            className={cn(
+              'text-sm h-7 min-w-7 flex items-center justify-center rounded-sm hover:bg-neutral-200/80',
+              editor?.getAttributes('textStyle').fontFamily === value &&
+                'bg-neutral-200/80'
+            )}
+            style={{ fontFamily: value }}
+            onClick={()=>{}}
+          >
+            <span className='text-xs'>{label}</span>
+          </button>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 interface ToolbarButtonProps {
   icon: LucideIcon
@@ -127,8 +248,10 @@ const Toolbar = () => {
       ))}
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
       {/* TODO: Font Family */}
+      <FontFamilyButton />
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
       {/* TODO: Heading */}
+      <HeadingLevelButton />
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
       {/* TODO: Font Size */}
       <Separator orientation='vertical' className='h-6 bg-neutral-300' />
